40f4c599b0184266bd2ce74c13b7ac9b948c9f99
Spiegel08 <108896685+Spiegel08@users.noreply.github.com>
2022-09-13 17:56:51 +0300

    Update 56_Merge_Intervals.py

--- 56_Merge_Intervals.py
+++ 56_Merge_Intervals.py
@@ -1,25 +1,28 @@
 
-intervals = [[1,3],[2,6],[8,10],[15,18],[17,24]]
+# intervals = [[1,3],[2,6],[8,10],[15,18]]
+
+# intervals = [[1,4],[4,5]]
+
+intervals = [[1,4]]
 
 listOfSets = [set(range(intervals[i][0], intervals[i][1] + 1)) for i in range(len(intervals))]
 
-theList = []
+listOfMergedSets = []
+
+setOfIndices = set()
 
 for i in range(len(listOfSets)):
     for j in range(i+1, len(listOfSets)):
         if min(listOfSets[i]) in listOfSets[j] or max(listOfSets[i]) in listOfSets[j]:
-            print(min(listOfSets[i]), max(listOfSets[i]), listOfSets[j])
-            theList.append(listOfSets[i] | listOfSets[j])
-        else:
-            theList.append(listOfSets[j])
+            listOfMergedSets.append((listOfSets[i] | listOfSets[j]))
+            setOfIndices |= {i, j}
 
+intervals_output = []
+for i in range(len(intervals)):
+    if i not in setOfIndices:
+        intervals_output.append(intervals[i])
 
-s = set(theList)
+for i in range(len(listOfMergedSets)):
+    intervals_output.append([min(listOfMergedSets[i]), max(listOfMergedSets[i])])
 
-[{1, 2, 3}, {2, 3, 4, 5, 6}, {8, 9, 10}, {16, 17, 18, 15}]
-
-print(listOfSets)
-
-print(theList)
-
-print(s)
+print(sorted(intervals_output))
